8f962bea9b2f09d85745ab36a2f919d19319d329
Anna Jaśkiewicz <ania.jaskiewicz.2001@gmail.com>
2023-05-18 09:47:11 +0200

    working on task editor and passing data - accelerating time error solved

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 import {Component} from "react";
 
 function Clock({minutes, seconds}) {
-    return(
+    return (
         <div>
             Pozostało {minutes}:{seconds}
         </div>
     )
+}
+
+class TaskEditor extends Component {
+
+    render() {
+        const {onTitleChange, onTimeChange} = this.props
+        return (
+            <div className='TaskEditor'>
+                <label> Co chcesz robić?<input onChange={onTitleChange} type="text"/></label>
+                <label> Przez ile minut?<input onChange={onTimeChange} type="number"/></label>
+                <button>Zatwierdź zmiany</button>
+            </div>
+        )
+    }
 }
 
 class TaskDisplayBox extends Component {
@@ -21,6 +35,7 @@ class TaskDisplayBox extends Component {
     }
 
     startTimer = () => {
+        clearInterval(this.intervalId)
         this.intervalId = window.setInterval(() => {
             this.setState((prevState) => ({
                 elapsedTimeInSeconds: prevState.elapsedTimeInSeconds + 0.1
@@ -64,7 +79,6 @@ class TaskDisplayBox extends Component {
     }
 
 
-
     render() {
         const {
             isPaused,
@@ -73,13 +87,17 @@ class TaskDisplayBox extends Component {
             elapsedTimeInSeconds,
         } = this.state
 
-        const totalTimeInSeconds = 600
+        const { title, totalTimeInMinutes } = this.props
+
+        const totalTimeInSeconds = totalTimeInMinutes * 60
         const timeLeftInSeconds = totalTimeInSeconds - elapsedTimeInSeconds
         const minutesLeft = Math.floor(timeLeftInSeconds / 60)
         const secondsLeft = Math.floor(timeLeftInSeconds % 60)
+
+
         return (
             <div className='TaskDisplayBox'>
-                <h2 className='task_name'>Uczę się Reacta</h2>
+                <h2 className='task_name'>{title}</h2>
                 <Clock minutes={minutesLeft} seconds={secondsLeft}/>
                 <div className={`ProgressBar ${isPaused ? 'inactive' : ''}`}>
                     <div className='progress'></div>
@@ -96,88 +114,38 @@ class TaskDisplayBox extends Component {
     }
 }
 
+class EditableTimeBox extends Component {
+    state = {
+        title: 'Uczę się Reacta',
+        totalTimeInMinutes: 5,
+    }
+
+    handleTitleChange = (event) => {
+        this.setState({title: event.target.value})
+    }
+
+    handleTimeChange = (event) => {
+        this.setState({totalTimeInMinutes: event.target.value})
+    }
+
+    render() {
+        const {title, totalTimeInMinutes } = this.state
+
+        return (
+            <>
+                <TaskEditor onTitleChange={this.handleTitleChange} onTimeChange={this.handleTimeChange}/>
+                <TaskDisplayBox title={title} totalTimeInMinutes={totalTimeInMinutes}/>
+            </>
+        )
+    }
+}
+
 function App() {
     return (
         <div className="App">
-            <TaskDisplayBox/>
+            <EditableTimeBox/>
         </div>
     );
 }
 
 export default App;
-
-// import React from "react";
-// import './App.css'
-//
-//
-// function Heading() {
-//     return (
-//         <h1>Pomodoro Clock</h1>
-//     )
-// }
-//
-// function TimeBoxEditor() {
-//     return (
-//         <div className='TimeBoxEditor'>
-//             <label>Co robisz?<input id='task_name_input' type="text" value=''/></label>
-//             <label>Ile minut? <input id='task_time_input' type="number"/></label>
-//             <button>Rozpocznij</button>
-//         </div>
-//     )
-// }
-//
-// class TimeBox extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             isRunning: true,
-//             isPaused: false,
-//             pausesCount: 0,
-//
-//         }
-//     }
-//
-//     handleStart() {
-//         this.setState({
-//             isRunning: true,
-//             isPaused: false,
-//         })
-//     }
-//
-//     handlePause() {
-//         this.setState({
-//             isRunning: false,
-//             isPaused: true
-//         })
-//     }
-//
-//
-//     render() {
-//         return (
-//             <div className='TimeBox'>
-//                 <h2 className='task_name'>Uczę się Reacta</h2>
-//                 <h3 className='time_left'>Pozostało: ....</h3>
-//                 <div className='ProgressBar'>
-//                     <div className='progress'></div>
-//                 </div>
-//                 <div className='control_buttons'>
-//                     <button onClick={this.handleStart}>Start</button>
-//                     <button onClick={this.handleStop}>Stop</button>
-//                     <button onClick={this.handlePause}>{this.isRunning ? 'Pauzuj' : 'Wznów'}</button>
-//                 </div>
-//                 <h3 className='breaks_counter'>Liczba przerw:</h3>
-//             </div>
-//         )
-//     }
-//
-// }
-//
-// export default function App() {
-//     return (
-//         <div className='App'>
-//             <Heading/>
-//             <TimeBoxEditor/>
-//             <TimeBox/>
-//         </div>
-//     )
-// }
